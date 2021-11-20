1a213f3e0d2b78f39ccf0233e9b922d62c4e9c3d
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-11-20 19:01:24 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/Caster.js
+++ src/js/Caster.js
@@ -8,21 +8,21 @@ export default class Caster {
     }
   
     get stoned() {
-      return this._stoned;
+      return this.stoneds;
     }
   
     set stoned(value) {
-      this._stoned = !!value;
+      this.stoneds = !!value;
     }
   
     get attack() {
       let reduction = 1 - (this.x - 1) * 0.1;
       reduction -= this.stoned ? Math.log2(this.x) * 0.05 : 0;
       reduction = (reduction > 0) ? reduction : 0;
-      return Math.round(this._attack * reduction);
+      return Math.round(this.attacks * reduction);
     }
   
     set attack(value) {
-      this._attack = value > 0 ? value : 0;
+      this.attacks = value > 0 ? value : 0;
     }
   }
