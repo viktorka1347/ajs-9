a79df142323ed6fbaade35f85ed0facf1989dfba
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-11-21 18:28:48 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/Caster.js
+++ src/js/Caster.js
@@ -1,28 +1,28 @@
 export default class Caster {
     constructor(name, type) {
-      this.name = name;
-      this.type = type;
-      this.attack = 0;
-      this.stoned = false;
-      this.x = 1;
+        this.name = name;
+        this.type = type;
+        this.attack = 0;
+        this.stoned = false;
+        this.x = 1;
     }
-  
+
     get stoned() {
-      return this.stoneds;
+        return this.stoneds;
     }
-  
+
     set stoned(value) {
-      this.stoneds = !!value;
+        this.stoneds = !!value;
     }
-  
+
     get attack() {
-      let reduction = 1 - (this.x - 1) * 0.1;
-      reduction -= this.stoned ? Math.log2(this.x) * 0.05 : 0;
-      reduction = (reduction > 0) ? reduction : 0;
-      return Math.round(this.attacks * reduction);
+        let reduction = 1 - (this.x - 1) * 0.1;
+        reduction -= this.stoned ? Math.log2(this.x) * 0.05 : 0;
+        reduction = (reduction > 0) ? reduction : 0;
+        return Math.round(this.attacks * reduction);
     }
-  
+
     set attack(value) {
-      this.attacks = value > 0 ? value : 0;
+        this.attacks = value > 0 ? value : 0;
     }
-  }
+}
