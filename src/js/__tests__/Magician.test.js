e8d55a591a4c7a556151f1663065aa08a0ea0ed8
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-11-21 18:24:59 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/__tests__/Magician.test.js
+++ src/js/__tests__/Magician.test.js
@@ -3,37 +3,37 @@ import Magician from '../Magician';
 const magician = new Magician('Петя');
 
 test('Должен создаваться маг', () => {
-  const result = {
-    name: 'Петя',
-    type: 'Magician',
-    _attack: 100,
-    _stoned: false,
-    x: 1,
-  };
-  expect(magician).toEqual(result);
+    const result = {
+        name: 'Петя',
+        type: 'Magician',
+        attacks: 100,
+        stoneds: false,
+        x: 1,
+    };
+    expect(magician).toEqual(result);
 });
 
 test.each([
-  [1, 100],
-  [2, 90],
-  [3, 80],
-  [11, 0],
+    [1, 100],
+    [2, 90],
+    [3, 80],
+    [11, 0],
 ])(
-  'Атака мага должна ослабляться с расстоянием', (x, attack) => {
-    magician.x = x;
-    expect(magician.attack).toBe(attack);
-  },
+    'Атака мага должна ослабляться с расстоянием', (x, attack) => {
+        magician.x = x;
+        expect(magician.attack).toBe(attack);
+    },
 );
 
 test.each([
-  [1, 100],
-  [2, 85],
-  [3, 72],
-  [11, 0],
+    [1, 100],
+    [2, 85],
+    [3, 72],
+    [11, 0],
 ])(
-  'Атака одурманенного мага должна дополнительно ослабляться', (x, attack) => {
-    magician.stoned = true;
-    magician.x = x;
-    expect(magician.attack).toBe(attack);
-  },
+    'Атака одурманенного мага должна дополнительно ослабляться', (x, attack) => {
+        magician.stoned = true;
+        magician.x = x;
+        expect(magician.attack).toBe(attack);
+    },
 );
