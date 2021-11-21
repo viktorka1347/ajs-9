1c2e663a56bedc0c2507c6b267c752bf597496d3
Viktoria Ulyanchenko <viktorka1347@gmail.com>
2021-11-21 18:30:09 +0300

    main
    Signed-off-by: Viktoria Ulyanchenko <viktorka1347@gmail.com>

--- src/js/__tests__/Caster.test.js
+++ src/js/__tests__/Caster.test.js
@@ -1,5 +1,5 @@
 import Caster from '../Caster';
 
 test('Должен создаваться объект', () => {
-  expect(new Caster('Изя', 'Daemon')).toBeDefined();
+    expect(new Caster('Изя', 'Daemon')).toBeDefined();
 });
